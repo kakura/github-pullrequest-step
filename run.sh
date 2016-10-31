ce523cfd728d519d7bc749691b7860cacc0e9363
shuta kakura <kakura@shutas-MacBook-Pro.local>
2016-10-31 14:27:18 +0900

    mod variable name

--- run.sh
+++ run.sh
@@ -1,48 +1,48 @@
 #!/bin/bash
 
-if [ ! -n "${WERCKER_GITHUB_PULLREQEST_USER}" ]; then
+if [ ! -n "${WERCKER_GITHUB_PULLREQUEST_USER}" ]; then
   error 'Please specify user property'
   exit 1
 fi
 
-if [ ! -n "${WERCKER_GITHUB_PULLREQEST_REPOSITORY}" ]; then
+if [ ! -n "${WERCKER_GITHUB_PULLREQUEST_REPOSITORY}" ]; then
   error 'Please specify repository property'
   exit 1
 fi
 
-if [ ! -n "${WERCKER_GITHUB_PULLREQEST_TOKEN}" ]; then
+if [ ! -n "${WERCKER_GITHUB_PULLREQUEST_TOKEN}" ]; then
   error 'Please specify token property'
   exit 1
 fi
 
-if [ ! -n "${WERCKER_GITHUB_PULLREQEST_TITLE}" ]; then
+if [ ! -n "${WERCKER_GITHUB_PULLREQUEST_TITLE}" ]; then
   error 'Please specify title property'
   exit 1
 fi
 
-if [ ! -n "${WERCKER_GITHUB_PULLREQEST_FROM}" ]; then
+if [ ! -n "${WERCKER_GITHUB_PULLREQUEST_FROM}" ]; then
   FROM='master'
 else
-  FROM=${WERCKER_GITHUB_PULLREQEST_FROM}
+  FROM=${WERCKER_GITHUB_PULLREQUEST_FROM}
 fi
 
-if [ ! -n "${WERCKER_GITHUB_PULLREQEST_TO}" ]; then
+if [ ! -n "${WERCKER_GITHUB_PULLREQUEST_TO}" ]; then
   TO='develop'
 else
-  TO=${WERCKER_GITHUB_PULLREQEST_TO}
+  TO=${WERCKER_GITHUB_PULLREQUEST_TO}
 fi
 
-if [ ! -n "${WERCKER_GITHUB_PULLREQEST_BODY}" ]; then
+if [ ! -n "${WERCKER_GITHUB_PULLREQUEST_BODY}" ]; then
   BODY='Please check this pull request.'
 else
-  BODY=${WERCKER_GITHUB_PULLREQEST_BODY}
+  BODY=${WERCKER_GITHUB_PULLREQUEST_BODY}
 fi
 
 
-USER=${WERCKER_GITHUB_PULLREQEST_USER}
-REPOSITORY=${WERCKER_GITHUB_PULLREQEST_REPOSITORY}
-TOKEN=${WERCKER_GITHUB_PULLREQEST_TOKEN}
-TITLE=${WERCKER_GITHUB_PULLREQEST_TITLE}
+USER=${WERCKER_GITHUB_PULLREQUEST_USER}
+REPOSITORY=${WERCKER_GITHUB_PULLREQUEST_REPOSITORY}
+TOKEN=${WERCKER_GITHUB_PULLREQUEST_TOKEN}
+TITLE=${WERCKER_GITHUB_PULLREQUEST_TITLE}
 
 
 curl -u "${USER}:${TOKEN}" -d '{"title": "${TITLE}","body": "${BODY}","head": "${USER}:${FROM}","base": "${TO}"}'  https://api.github.com/repos/${USER}/${REPOSITORY}/pulls
